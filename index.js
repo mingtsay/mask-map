59c26f9e09f2bfebe3464c571c83b933175157b4
Ming Tsay <mt@moztw.org>
2020-03-08 01:14:52 +0800

    verbose listening host and port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
-const router = require('./router');
 const sgr = {
-  bold: '\033[1m',
-  reset: '\033[0m',
+    bold: '\033[1m',
+    reset: '\033[0m',
 };
-const serv = (new (require('koa')))
+
+const router = require('./router');
+const app = (new (require('koa')))
     .use(require('koa-logger')())
     .use(require('koa-response-time')())
     .use(require('koa-conditional-get')())
@@ -13,6 +14,10 @@ const serv = (new (require('koa')))
     .use(require('koa-static')('./static'))
     .use(router.routes())
     .use(router.allowedMethods())
-    .listen(process.env.LISTEN_PORT);
-
-if (process.env.VERBOSE == '1') console.log(`${sgr.bold}INFO:${sgr.reset} We are listening on port ${serv.address().port}.`);
+    .listen(process.env.LISTEN_PORT, process.env.LISTEN_HOST, () => {
+        const {address, port, family} = app.address();
+        const hostname = family === 'IPv6' ? `[${address}]` : address;
+        const protocol = app.addContext ? 'https' : 'http';
+        if (process.env.VERBOSE)
+            console.log(`${sgr.bold}INFO:${sgr.reset} Listening on ${protocol}://${hostname}:${port}…`);
+    });
