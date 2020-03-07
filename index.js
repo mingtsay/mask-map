e6fe5243c934b7f2f658c642215eff5df94e9e3c
pan93412 <pan93412@gmail.com>
2020-03-07 18:39:22 +0800

    feat(server): show the port the server listened on

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
 const router = require('./router');
-
-(new (require('koa')))
+const sgr = {
+  bold: '\033[1m',
+  reset: '\033[0m',
+};
+const serv = (new (require('koa')))
     .use(require('koa-logger')())
     .use(require('koa-response-time')())
     .use(require('koa-conditional-get')())
@@ -10,3 +13,5 @@ const router = require('./router');
     .use(router.routes())
     .use(router.allowedMethods())
     .listen(process.env.LISTEN_PORT);
+
+console.log(`${sgr.bold}INFO:${sgr.reset} We are listening on port ${serv.address().port}.`);
