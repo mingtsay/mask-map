1b9f00c71bb5799fd5eeaf5b27f36f1b0664ea93
Ming Tsay <mt@moztw.org>
2020-10-20 18:28:13 +0800

    update gulp file and versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,61 @@
-const { 
-  series, parallel, src, dest
-} = require('gulp');
-const terser = require('gulp-terser');
-const cleanCSS = require('gulp-clean-css');
-const htmlmin = require('gulp-htmlmin');
-const del = require('del');
+const { dest, parallel, series, src, task } = require('gulp')
+const terser = require('gulp-terser')
+const cleanCss = require('gulp-clean-css')
+const htmlMin = require('gulp-htmlmin')
+const del = require('del')
 
-exports.clean = function clean() {
-  return del('static');
+const addTask = ({ name = '', description = '', fn }) => {
+    if (name) fn.displayName = name
+    if (description) fn.description = description
+    task(fn)
 }
-exports.js = function js() {
-  return src('src/js/*.js')
-    .pipe(terser())
-    .pipe(dest('static/js'));
-};
-exports.css = function css() {
-  return src('src/css/*.css')
-    .pipe(cleanCSS())
-    .pipe(dest('static/css'));
-};
-exports.html = function html() {
-  return src('src/index.html')
-    .pipe(htmlmin({
-      collapseBooleanAttributes: true,
-      collapseInlineTagWhitespace: true,
-      collapseWhitespace: true,
-      decodeEntities: true,
-      quoteCharacter: "'",
-      removeAttributeQuotes: true,
-      removeComments: true,
-      removeRedundantAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-    }))
-    .pipe(dest('static'));
-}
-exports.default = series(exports.clean, parallel(exports.js, exports.css, exports.html));
+
+addTask({
+    name: 'clean',
+    description: 'Clean output files',
+    fn: cb => del('static')
+        .then(() => cb()),
+})
+
+addTask({
+    name: 'js',
+    description: 'Terser JavaScript files',
+    fn: cb => src('src/js/**/*.js')
+        .pipe(terser())
+        .pipe(dest('static/js'))
+        .on('end', cb),
+})
+
+addTask({
+    name: 'css',
+    description: 'Clean CSS files',
+    fn: cb => src('src/css/**/*.css')
+        .pipe(cleanCss())
+        .pipe(dest('static/css'))
+        .on('end', cb),
+})
+
+addTask({
+    name: 'html',
+    description: 'Minify HTML files',
+    fn: cb => src('src/**/*.html')
+        .pipe(htmlMin({
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            collapseWhitespace: true,
+            decodeEntities: true,
+            quoteCharacter: '\'',
+            removeAttributeQuotes: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+        }))
+        .pipe(dest('static'))
+        .on('end', cb),
+})
+
+addTask({
+    name: 'default',
+    description: 'Rebuild all files',
+    fn: series('clean', parallel('js', 'css', 'html')),
+})
